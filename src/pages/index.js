84d489307044b99ce4b78ea5c5a7bd5fdd8ca148
Michael Babin <info@micb.dev>
2023-01-21 08:23:50 -0500

    Project structure for home and nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import * as React from "react"
+import React from "react";
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
